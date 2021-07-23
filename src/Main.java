5019f24bf90ac48f56607ee20cfd8c632b5c2a8b
Alistair Chambers <38084528+Cambrian34@users.noreply.github.com>
2021-07-22 23:08:03 -0400

    Minor changes.

--- src/Main.java
+++ src/Main.java
@@ -13,16 +13,16 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     // choice 1 = hex, choice 2 = decimal, choice 3 = binary,choice 4 = base 1
-    Boolean inchoice1= false;
-    Boolean inchoice2= false;
-    Boolean inchoice3= false;
-    Boolean inchoice4= false;
+    Boolean inchoice1 = false;
+    Boolean inchoice2 = false;
+    Boolean inchoice3 = false;
+    Boolean inchoice4 = false;
 
-    Boolean outchoice1= false;
-    Boolean outchoice2= false;
-    Boolean outchoice3= false;
-    Boolean outchoice4= false;
-    int m,k;
+    Boolean outchoice1 = false;
+    Boolean outchoice2 = false;
+    Boolean outchoice3 = false;
+    Boolean outchoice4 = false;
+    int     m, k;
     String g, out;
     static RadioButton button1;
     static RadioButton button2;
@@ -39,7 +39,7 @@ public class Main extends Application {
         HBox input_hor = new HBox();
         TextField input1 = new TextField();
         Text Input = new Text("INPUT  ");
-        input_hor.getChildren().addAll(Input,input1);
+        input_hor.getChildren().addAll(Input, input1);
         ToggleGroup from = new ToggleGroup();
         button1 = new RadioButton("Hexadecimal / Base 16");
         button2 = new RadioButton("Decimal / Base 10");
@@ -51,7 +51,7 @@ public class Main extends Application {
         button4.setToggleGroup(from);
         Text lfrom = new Text("FROM \t");
         HBox from1 = new HBox();
-        from1.getChildren().addAll(lfrom,button1,button2,button3,button4);
+        from1.getChildren().addAll(lfrom, button1, button2, button3, button4);
 
         ToggleGroup to = new ToggleGroup();
         button5 = new RadioButton("Hexadecimal / Base 16");
@@ -64,34 +64,33 @@ public class Main extends Application {
         button8.setToggleGroup(to);
         Text l2from = new Text("TO \t");
         HBox to1 = new HBox();
-        to1.getChildren().addAll(l2from,button5,button6,button7,button8);
+        to1.getChildren().addAll(l2from, button5, button6, button7, button8);
 
         VBox inout = new VBox();
-        inout.getChildren().addAll(from1,to1);
+        inout.getChildren().addAll(from1, to1);
 
         Button convert = new Button("Convert ");
 
         TextField output = new TextField();
         HBox Output_hor = new HBox();
         Text Output = new Text("OUTPUT  ");
-        Output_hor.getChildren().addAll(Output,output);
+        Output_hor.getChildren().addAll(Output, output);
         VBox ui = new VBox();
 
-        ui.getChildren().addAll(input_hor,inout, convert, Output_hor);
+        ui.getChildren().addAll(input_hor, inout, convert, Output_hor);
 
         Group root = new Group(ui);
         from.selectedToggleProperty().addListener((ov, old_toggle, new_toggle) -> {
             if (from.getSelectedToggle() == button1) {
                 inchoice1 = true;
 
-            }
-            else if (from.getSelectedToggle() == button2){
+            } else if (from.getSelectedToggle() == button2) {
                 inchoice2 = true;
 
-            }else if(from.getSelectedToggle() ==button3){
+            } else if (from.getSelectedToggle() == button3) {
                 inchoice3 = true;
 
-            }else if(from.getSelectedToggle() == button4) {
+            } else if (from.getSelectedToggle() == button4) {
                 inchoice4 = true;
             }
         });
@@ -99,13 +98,13 @@ public class Main extends Application {
             if (to.getSelectedToggle() == button5) {
                 outchoice1 = true;
 
-            }else if (to.getSelectedToggle() == button6){
+            } else if (to.getSelectedToggle() == button6) {
                 outchoice2 = true;
 
-            }else if(to.getSelectedToggle() ==button7){
+            } else if (to.getSelectedToggle() == button7) {
                 outchoice3 = true;
 
-            }else if(to.getSelectedToggle() == button8) {
+            } else if (to.getSelectedToggle() == button8) {
                 outchoice4 = true;
 
             }
@@ -124,64 +123,58 @@ public class Main extends Application {
             //Add functionality to radio buttons and use them to choose the output value here.
 
             // if else "from"
-            if (inchoice1 == true && inchoice2 == false && inchoice3 == false && inchoice4 == false ){
+            if (inchoice1 == true && inchoice2 == false && inchoice3 == false && inchoice4 == false) {
                 System.out.println("1");
                 inchoice1 = false;
 
-            }
-            else if (inchoice2 == true){
+            } else if (inchoice2 == true) {
 
                 System.out.println("2");
                 inchoice2 = false;
-            }
-            else if (inchoice3 == true){
+            } else if (inchoice3 == true) {
                 System.out.println("3");
                 inchoice3 = false;
 
-            }
-            else if (inchoice4 == true){
+            } else if (inchoice4 == true) {
 
                 inchoice4 = false;
                 System.out.println("4");
-            }else {
+            } else {
                 System.out.println("needs tuning");
             }
             // if else  "to"
-            if (outchoice1 == true && outchoice2 == false && outchoice3 == false && outchoice4 == false ){
+            if (outchoice1 == true && outchoice2 == false && outchoice3 == false && outchoice4 == false) {
                 System.out.println("1");
                 outchoice1 = false;
 
-            }
-            else if (outchoice2 == true){
+            } else if (outchoice2 == true) {
 
                 System.out.println("2");
                 outchoice2 = false;
-            }
-            else if (outchoice3 == true){
+            } else if (outchoice3 == true) {
                 System.out.println("3");
                 outchoice3 = false;
 
-            }
-            else if (outchoice4 == true){
+            } else if (outchoice4 == true) {
 
                 outchoice4 = false;
                 System.out.println("4");
-            }else {
+            } else {
                 System.out.println("needs tuning");
             }
-
 
 
             String l = String.valueOf(k);
             output.setText(l);
 
         });
-        Scene scene = new Scene(root,600,400);
+        Scene scene = new Scene(root, 600, 400);
         primaryStage.setScene(scene);
         primaryStage.setTitle("");
         primaryStage.show();
     }
-    public void clearselection(){
+
+    public void clearselection() {
 
         button1.setSelected(false);
         button2.setSelected(false);
@@ -192,42 +185,47 @@ public class Main extends Application {
         button7.setSelected(false);
         button8.setSelected(false);
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         launch(args);
     }
+
     public void bintodec() {
 //Converts from binary to decimal
         k = Integer.parseInt(g, 2);
     }
-    public void dectobin(){
+
+    public void dectobin() {
         // Converts from decimal to binary
         out = (Integer.toBinaryString(m));
     }
-    public void hextodec(){
+
+    public void hextodec() {
         // Converts from hex to decimal
-        k = Integer.parseInt(g,16);
+        k = Integer.parseInt(g, 16);
     }
-    public void dectohex(){
+
+    public void dectohex() {
         // Converts from decimal to hex
         out = Integer.toHexString(m);
     }
 
-    public void bintob2(){
+    public void bintob2() {
         //Converts from binary to binary
     }
-    public void bintob1(){
+
+    public void bintob1() {
         //Converts from binary to base 1
     }
-    public void bintohex(){
+
+    public void bintohex() {
         //Converts from binary to hexadecimal
     }
 }
+
 class t implements Runnable {
 
-    private int var;
-
     public t(int var) {
-        this.var = var;
     }
 
     public void run() {
